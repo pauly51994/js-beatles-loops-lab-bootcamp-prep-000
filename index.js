6a82c47dcc4edc78389a452147c27c328e3d4cae
Paul Yun <p.yun1994@gmail.com>
2019-06-21 23:14:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,10 +8,19 @@ function theBeatlesPlay(musicians, instruments){
 }
 
 function johnLennonFacts(facts){
-  var i = 0;
+  let i = 0;
   while(i < facts.length){
     facts[i] += "!!!";
     i++;
   }
   return facts;
+}
+
+function iLoveTheBeatles(num){
+  let arr = [];
+  do {
+    arr.push("I love the Beatles!");
+    num ++;
+  } while (num < 15);
+  return arr;
 }
