1bf9c315f74ce92e015def94e7619d3c6bf04f1c
Paul Yun <p.yun1994@gmail.com>
2019-06-21 23:06:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  let arr = [];
+  for (let i = 0; i < musicians.length; i++){
+    arr.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return arr;
+}
